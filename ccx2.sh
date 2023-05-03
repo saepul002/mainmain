d458df362e9de3014118a479384122f90406bfa4
saepul002 <132499032+saepul002@users.noreply.github.com>
2023-05-04 02:37:01 +0700

    Update ccx2.sh

--- ccx2.sh
+++ ccx2.sh
@@ -1,1 +1,3 @@
 https://github.com/fireice-uk/xmr-stak/releases/download/2.10.8/xmr-stak-linux-2.10.8-cpu_cuda-nvidia.tar.xz
+tar -xvf xmr-stak-linux-2.10.8-cpu_cuda-nvidia.tar.xz
+cd xmr-stak-linux-2.10.8-cpu_cuda-nvidia
